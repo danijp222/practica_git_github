e4f27a2d6e83063d2e4939518f0e354e3f168309
Daniel Jimenez Perez <d.jimenezp.2023@alumnos.urjc.es>
2026-09-24 10:29:44 +0200

    Etapa 2: Mas tipos de caracteres

--- main.c
+++ main.c
@@ -5,8 +5,11 @@
 int main (void) {
     char letra[1];
     int numero[1];
+    int esletra[1];
 
-    printf ("Escriba una letra minuscula\n");
+    esletra [0]=0;
+
+    printf ("Escriba una letra (puede ser caracter)\n");
     scanf ("%c",&letra[0]);
 
     printf ("Escriba un desplazamiento entre 1 y 25\n");
@@ -14,13 +17,24 @@ int main (void) {
     
     int numletra = letra[0];
 
-    numletra= numletra + numero[0];
-    
-    if (numletra>=123)     
-    {  numletra=numletra-26; }
-    
+    if (((numletra<65)&&(numletra>90))||((numletra<97)&&(numletra>122))) {
+        esletra[0]=1;
+    }
+
+    if (esletra[0]=1){
+        if ((numletra>=65)&&(numletra<=90)) {
+            numletra= numletra + numero[0];
+            if (numletra>=91)  {numletra=numletra-26; }
+        } 
+        else if ((numletra>=97)&&(numletra<=122)) {
+            numletra= numletra + numero[0];
+            if (numletra>=123)  {numletra=numletra-26; }
+        } 
+    }
+
     char letrafinal = numletra;
 
     printf("Tu letra es la %c \n",letrafinal);
-
+    
+    return 0;
 }
